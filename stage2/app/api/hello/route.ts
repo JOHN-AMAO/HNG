e8ef8deabc9ee357004b84f1e7bdd936e04d243b
JOHN-AMAO <amaojohn767@gmail.com>
2024-07-04 05:15:48 +0100

    created route to get ip adress and onboard user

---
+++ stage2/app/api/hello/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import IPinfoWrapper, { IPinfo } from "node-ipinfo";
+import requestIp from "request-ip";
+
+const ipinfoWrapper = new IPinfoWrapper("95b0bc89ff9156");
+
+export async function GET(req: any) {
+  // Get the client's IP address
+  const ip = (await requestIp.getClientIp(req)) || "";
+
+  // Look up the IP information
+  const response: IPinfo = await ipinfoWrapper.lookupIp(ip);
+
+  // Get the visitor_name from query parameters
+  const url = new URL(req.url);
+  const visitorName = url.searchParams.get("visitor_name") || "Visitor";
+
+  // Return the response as JSON
+  return NextResponse.json({
+    client_ip: response.ip,
+    location: response.city,
+    greeting: `Hello, ${visitorName}, the temperature is 24.3 degrees in ${response.city}!`,
+  });
+}
